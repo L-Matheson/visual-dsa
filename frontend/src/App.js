7f117ac38117fc5cbb925a1afa004f3bc47f82d0
lmatheson2026 <144724348+lmatheson2026@users.noreply.github.com>
2025-08-23 12:27:17 -0400

    Top bar work

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,47 @@
+import { ColorModeContext, useMode } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import Topbar from "./scenes/global/Topbar";
+import Dashboard from "./scenes/dashboard";
+import Sidebar from "./scenes/global/Sidebar";
+import Team from "./scenes/team";
+import Invoices from "./scenes/invoices";
+import Bar from "./scenes/bar";
+import Form from "./scenes/form";
+import Line from "./scenes/line";
+import Pie from "./scenes/pie";
+import FAQ from "./scenes/faq";
+import Geography from "./scenes/geography";
+import Calendar from "./scenes/calendar";
+import Contacts from "./scenes/contacts"
+
 function App() {
+  const [theme, colorMode] = useMode();
   return (
-    <div className="app">
-      <h1>Welcome to Visual DSA</h1>
-    </div>
+    // Since this is a context, .provider is used to wrap all the child components, then supply the value
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        {/* On theme change, CssBaseLine resets css values to default */}
+        <CssBaseline />
+        <div className="app">
+          <main className="content">
+            <Topbar />
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              {/* <Route path="/team" element={<Team />} /> */}
+              {/* <Rotue path="/contacts" element={<Contacts />}/> */}
+              {/* <Route path="/invoices" element={<Invoices />} /> */}
+              {/* <Route path="/form" element={<Form />} /> */}
+              {/* <Route path="/bar" element={<Bar />} /> */}
+              {/* <Route path="/pie" element={<Pie />} /> */}
+              {/* <Route path="/line" element={<Line />} /> */}
+              {/* <Route path="/faq" element={<FAQ />} /> */}
+              {/* <Route path="/geography" element={<Geography />} /> */}
+              {/* <Route path="/calendar" element={<Calendar/>}/> */}
+            </Routes>
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
--- frontend/src/scenes/global/Topbar.jsx
+++ frontend/src/scenes/global/Topbar.jsx
@@ -1,6 +1,58 @@
+import { Box, IconButton, useTheme } from "@mui/material";
+import { useContext } from "react";
+import { ColorModeContext, tokens } from "../../theme";
+import InputBase from "@mui/material/InputBase";
+import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import NotificationsOutlinedIcon from "@mui/icons-material/Notifications";
+import SettingsOutlinedIcon from "@mui/icons-material/Settings";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import SearchIcon from "@mui/icons-material/SearchOutlined";
 
 const Topbar = () => {
-    return <div>Topbar</div>
-}
+  // grabs theme
+  const theme = useTheme();
+  // Allows for colors to remain consistant, also allows for dark mode
+  const colors = tokens(theme.palette.mode);
+  const colorMode = useContext(ColorModeContext);
+
+  return (
+    <Box display="Flex" justifyContent="space-between" p={2}>
+      {/* Search Bar */}
+      <Box
+        display="flex"
+        backgroundColor={colors.primary[400]}
+        borderRadius="3px"
+      >
+        <InputBase style={{ ml: 2, flex: 1 }} placeholder="Search">
+          <IconButton type="button" style={{ p: 1 }}>
+            {" "}
+            <SearchIcon />{" "}
+          </IconButton>
+        </InputBase>
+      </Box>
+
+      {/* Icons */}
+      <Box display="flex">
+        <IconButton onClick={colorMode.toggleColorMode}>
+          {theme.palette.mode === "dark" ? (
+            <DarkModeOutlinedIcon />
+          ) : (
+            <LightModeOutlinedIcon />
+          )}
+        </IconButton>
+        <IconButton>
+          <NotificationsOutlinedIcon />
+        </IconButton>
+        <IconButton>
+          <SettingsOutlinedIcon />
+        </IconButton>
+        <IconButton>
+          <PersonOutlinedIcon />
+        </IconButton>
+      </Box>
+    </Box>
+  );
+};
 
 export default Topbar;
